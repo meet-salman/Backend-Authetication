d27135685b81a120e635373495101af0ec7ff830
meet-salman <say2salmanahmed@gmail.com>
2026-08-28 09:44:16 +0500

    Password Hashing Added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,43 +1,57 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 async function registerUser(req, res) {
 
     const { username, password, email } = req.body;
 
-    const isUserExist = await userModel.findOne({
-        $or: [
-            { email },
-            { username }
-        ]
-    });
+    try {
+        // Check user exist or not
+        const isUserExist = await userModel.findOne({
+            $or: [
+                { email },
+                { username }
+            ]
+        });
 
-    // Check user exist or not
-    if (isUserExist) {
-        return res.status(409).json({
-            message: 'User already exists'
+        if (isUserExist) {
+            return res.status(409).json({
+                message: 'User already exists'
+            })
+        }
+
+        // Paassword hashing
+        const salt = await bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = await bcrypt.hashSync(password, salt);
+
+        // Create new user
+        const newUser = await userModel.create({
+            username,
+            password: hashedPassword,
+            email
+        });
+
+        // Generate JWT token 
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+        res.cookie('token', token);
+
+
+        res.status(201).json({
+            message: 'User registered successfully',
+            user: newUser
+        })
+
+    }
+    catch (error) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
         })
     }
-
-    // if (isUsernameExist) {
-    //     return res.status(409).json({
-    //         message: 'Username already exists'
-    //     })
-    // }
-
-    const newUser = await userModel.create({ username, password, email });
-
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-
-    res.cookie('token', token);
-
-    res.status(201).json({
-        message: 'User registered successfully',
-        user: newUser
-    })
-
-
 
 }
 
